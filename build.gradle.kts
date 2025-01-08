fbf86726c2b98838db385f73a844056e787a5df1
Kim Munjin <reminisce057@gmail.com>
2025-01-08 15:09:03 +0900

    major: major 버전업 테스트5

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ publishing {
         maven {
             name = "GitHubPackages"
             // url은 https://maven.pkg.github.com/YOUR_ORGANIZATION/githun repository 이름
-            url = uri("https://maven.pkg.github.com/KimMunjin/utils")
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
             credentials {
                 username = System.getenv("GITHUB_ACTOR") // workflows를 실행시킨 사용자 자동 설정
                 password = System.getenv("GITHUB_TOKEN") // Actions에서 자동 생성되는 토큰
