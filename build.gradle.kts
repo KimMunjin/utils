ef5c56eba6b27ec5232b104916f0ea1b44acc862
Kim Munjin <reminisce057@gmail.com>
2025-01-08 14:41:10 +0900

    refactor : publish 리팩토링

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,8 +57,8 @@ publishing {
             // url은 https://maven.pkg.github.com/YOUR_ORGANIZATION/githun repository 이름
             url = uri("https://maven.pkg.github.com/kun-9/komca-common-utils")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR") // workflows를 실행시킨 사용자 자동 설정
+                password = System.getenv("GITHUB_TOKEN") // Actions에서 자동 생성되는 토큰
             }
         }
     }
